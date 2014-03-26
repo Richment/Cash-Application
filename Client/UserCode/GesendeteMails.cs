24cab3d792d0a3d40eeb754bb770d0b0b09cc4f7
Richment <richment@hotmail.com>
2014-03-26 23:47:29 +0100

    narf

---
+++ Client/UserCode/GesendeteMails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+	public partial class GesendeteMails
+	{
+		partial void Resend_CanExecute(ref bool result)
+		{
+			result = OutgoingMailSet.SelectedItem != null;
+		}
+
+		partial void Resend_Execute()
+		{
+			OutgoingMailSet.SelectedItem.Sended = DateTime.Now;
+			Save();
+			Refresh();
+		}
+	}
+}
